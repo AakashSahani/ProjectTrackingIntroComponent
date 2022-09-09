ac24074194d854bdc1eb49afd7108e621568bd04
Aakash Sahani <sahaniaakash120@gmail.com>
2022-09-08 21:08:58 -0400

    Added JS for Menu

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,15 @@ const hamburger = document.querySelector('.hamburger');
 const navmenu = document.querySelector('.nav-item');
 
 hamburger.addEventListener('click', () => {
-	hamburger.classList.add('active');
-	navmenu.classList.add('active');
+	menudisplay();
 });
+
+function menudisplay() {
+	if (navmenu.style.visibility === 'visible') {
+		navmenu.style.visibility = 'hidden';
+		hamburger.src = './images/icon-hamburger.svg';
+	} else {
+		navmenu.style.visibility = 'visible';
+		hamburger.src = './images/icon-close.svg';
+	}
+}
